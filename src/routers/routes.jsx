eff2007a7643114034c8ff8ff6de9789ec4c4ac5
Franklin369 <codigo369.com@gmail.com>
2023-09-22 06:30:05 -0500

    first commit

---
+++ src/routers/routes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Login, Home, ProtectedRoute, UserAuth,Configuracion,Categorias,Movimientos } from "../index";
+export function MyRoutes() {
+  const { user } = UserAuth();
+  return (
+ 
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute user={user} redirectTo="/login" />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/configurar" element={<Configuracion />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/movimientos" element={<Movimientos />} />
+        </Route>
+      </Routes>
+  
+  );
+}
